c80be31dbd20d8dc15bcf8e2efaee2a572d7a5a4
mariam samy makram <mariam.samy.mrmr@gmail.com>
2019-10-09 11:26:44 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ButtonsModule } from 'ngx-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { headerComponent } from "./header/header.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    headerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    BrowserAnimationsModule,
+    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
